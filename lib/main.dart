cfd9901f5c8557ca2e5ceb3c15840d19e43030cd
marcuskgu <guskarmajz@student.gu.se>
2021-11-15 20:03:43 +0100

    Fixat knappar, view för lägga till tasks, resten fixas i steg 2

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.grey,
       ),
       home: MyHomePage(),
+      debugShowCheckedModeBanner: false, // ful och äcklig, bort med dig
     );
   }
 }
@@ -27,6 +28,12 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('TIG169 TODO'),
         centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: null,
+            icon: Icon(Icons.more_vert),
+          )
+        ],
       ),
       body: Center(
         child: Container(
@@ -36,44 +43,82 @@ class MyHomePage extends StatelessWidget {
             child: Column(
           children: [
             // fixa nycklar till parametrarna kanske?
-            TodoObject('Write a book', false),
-            TodoObject('Do homework', false),
-            TodoObject('Tidy room', true),
-            TodoObject('Watch TV', false),
-            TodoObject('Nap', false),
-            TodoObject('Shop groceries', false),
-            TodoObject('Have fun', false),
-            TodoObject('Meditate', false),
+            todoObject('Write a book', false),
+            todoObject('Do homework', false),
+            todoObject('Tidy room', true),
+            todoObject('Watch TV', false),
+            todoObject('Nap', false),
+            todoObject('Shop groceries', false),
+            todoObject('Have fun', false),
+            todoObject('Meditate', false),
           ],
         )),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => AddTaskScreen()));
+        },
+        child: const Icon(Icons.add),
+        backgroundColor: Colors.grey,
+      ),
+    );
+  }
+}
+
+class AddTaskScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add new TODO'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.all(20.0),
+            child: TextField(
+              decoration: InputDecoration(
+                border: UnderlineInputBorder(),
+                hintText: 'New TODO',
+              ),
+            ),
+          ),
+          TextButton.icon(
+            onPressed: () {},
+            icon: Icon(Icons.add, size: 18),
+            label: Text("Add TODO"),
+          )
+        ],
       ),
     );
   }
 }
 
 // Ett todo-objekt
-Container TodoObject(String TodoText, bool Completed) {
+Container todoObject(String TodoText, bool Completed) {
   return Container(
     width: double.infinity,
     height: 72,
-    decoration: TodoBorder(),
+    decoration: todoBorder(),
     child: Row(
       children: [
-        CheckBox(Completed),
+        checkBox(Completed),
         Text(TodoText,
             // borde kanske brytas ut till en egen funktion med if-sats istället
             style: (Completed)
                 ? TextStyle(decoration: TextDecoration.lineThrough)
                 : null),
         Spacer(),
-        TrashCan()
+        trashCan()
       ],
     ),
   );
 }
 
-// Checkboxen till todo-objektet TodoObject
-Container CheckBox(bool Completed) {
+// Checkboxen till todo-objektet todoObject
+Container checkBox(bool Completed) {
   return Container(
     width: 72,
     child: Center(
@@ -86,7 +131,7 @@ Container CheckBox(bool Completed) {
 }
 
 // Papperskorg till todo-objektet
-Container TrashCan() {
+Container trashCan() {
   return Container(
     width: 72,
     child: Center(
@@ -95,94 +140,10 @@ Container TrashCan() {
   );
 }
 
-BoxDecoration TodoBorder() {
+BoxDecoration todoBorder() {
   return const BoxDecoration(
-      border: Border(bottom: BorderSide(width: 1.0, color: Colors.grey)));
+    border: Border(
+      bottom: BorderSide(width: 1.0, color: Colors.grey),
+    ),
+  );
 }
-
- /*
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
-*/
